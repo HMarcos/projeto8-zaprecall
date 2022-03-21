13dc0aab31a288b71af60f35248d5255b82ad114
Marcos Henrique <marcosmarcone48@gmail.com>
2022-03-21 00:53:04 -0300

    função para atualizar o progresso de forma dinâmica

--- src/componentes/TelaRecall.js
+++ src/componentes/TelaRecall.js
@@ -10,22 +10,41 @@ function TelaRecall() {
         {
             numeroDeFlashcards: 4,
             flashcardsRespondidos: 0,
-            icones: ["checkmark-circle", "checkmark-circle", "checkmark-circle", "checkmark-circle"],
-            respostas: ["nao-lembrei"]
+            icones: [],
+            respostas: [],
         })
-
 
     const [finalizado, setFinalizado] = useState(false);
 
-    if (progresso.respostas.length === progresso.numeroDeFlashcards) {
-        setFinalizado(true);
+    function atualizarProgresso(infoProgresso) {
+        const { icone, resposta } = infoProgresso;
+        const flashcardsRespondidosAtualizados = progresso.flashcardsRespondidos + 1;
+        const iconesAtualizados = [...progresso.icones, icone];
+        const respostasAtualizadas = [...progresso.respostas, resposta];
+        setProgresso({
+            ...progresso,
+            flashcardsRespondidos: flashcardsRespondidosAtualizados,
+            icones: iconesAtualizados,
+            respostas: respostasAtualizadas
+        });
+
+        if (flashcardsRespondidosAtualizados === progresso.numeroDeFlashcards) {
+            setFinalizado(true);
+        }
     }
+
+
+
+
 
     return (
         <>
             <header>
                 <img src={LogoZapRecall} alt="logo-zap-recall" />
-                <h1>ZapRecall</h1>
+                <h1 onClick={() => {
+                    console.log("teste");
+                    atualizarProgresso({ icone: "checkmark-circle", resposta: "nao-lembrei" });
+                }}>ZapRecall</h1>
             </header>
 
             <main className="flashcards">
